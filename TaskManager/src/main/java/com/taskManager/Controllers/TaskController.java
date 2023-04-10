e4ca12ff485d5d93762744016679bb45bfe6eb16
Renan Prazeres Junkes <34254218+RenanPrazeresJunkes@users.noreply.github.com>
2023-04-10 14:55:05 -0300

    Delete adicionado

--- TaskManager/src/main/java/com/taskManager/Controllers/TaskController.java
+++ TaskManager/src/main/java/com/taskManager/Controllers/TaskController.java
@@ -2,10 +2,7 @@ package com.taskManager.Controllers;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.taskManager.Task;
 import com.taskManager.TaskRepository;
@@ -22,10 +19,19 @@ public class TaskController {
 	public List<Task> getAllTasks() {
 		return repository.findAll();
 	}
-	
+
+	@GetMapping("/task/{id}")
+	public Task getTaskID(@PathVariable Long id){
+		return repository.findById(id).get();
+	}
+
 	@PostMapping("/task")
 	public Task saveTask(@RequestBody Task task) {
 		return repository.save(task);
 	}
-	
+
+	@DeleteMapping("/task/{id}")
+	public void deleteTask(@PathVariable Long id){
+		repository.deleteById(id);
+	}
 }
